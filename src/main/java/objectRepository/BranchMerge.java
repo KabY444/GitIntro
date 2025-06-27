1782ec5f06ffd0b7575a00a696326cec28655fd4
Kanishk <bawejakanishk17@gmail.com>
2025-06-28 03:22:19 +0530

    New Branch Commit

---
+++ src/main/java/objectRepository/BranchMerge.java
@@ -0,0 +1,16 @@
+package objectRepository;
+
+public class BranchMerge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a = "test";
+		String s1 = new String("Kanishk");
+		System.out.println(s1);
+		
+		System.out.println("New class added in branch_KB");
+
+	}
+
+}
